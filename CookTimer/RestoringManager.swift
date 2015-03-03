0af77ef2fb5bdbe506761f598faff4ba42132b11
岩本果純 <iwamotokasumi@iwamotokajun-no-MacBook-Air.local>
2015-03-03 15:20:12 +0900

    first commit

---
+++ CookTimer/RestoringManager.swift
@@ -0,0 +1,13 @@
+//
+//  RestoringManager.swift
+//  CookTimer
+//
+//  Created by 岩本果純 on 2015/03/01.
+//  Copyright (c) 2015年 KasumiIwamoto. All rights reserved.
+//
+
+import UIKit
+
+class RestoringManager: NSObject {
+   
+}
